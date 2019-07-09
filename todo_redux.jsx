59bd4687ace47fc5fb60e39b8818472173400936
Joe Pell <jpell3130@gmail.com>
2019-07-09 16:51:16 -0400

    now renders Root (which renders App wrapped with react-redux's Provider)

--- todo_redux.jsx
+++ todo_redux.jsx
@@ -2,14 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import createStore from './frontend/store/store';
 import rootReducer from './frontend/reducers/rootReducer';
-
-const Root = props => {
-  return <h1>WORKING</h1>;
-};
+import Root from './frontend/components/root';
 
 document.addEventListener('DOMContentLoaded', () => {
   const content = document.getElementById('content');
-  ReactDOM.render(<Root />, content);
+  ReactDOM.render(<Root store={createStore} />, content);
 });
-
-window.store = createStore;

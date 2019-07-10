b37695342c28e636092e382cf43df30c32e23c3a
Joe Pell <jpell3130@gmail.com>
2019-07-10 12:11:14 -0400

    fixed typo

--- frontend/actions/steps_actions.js
+++ frontend/actions/steps_actions.js
@@ -1,6 +1,6 @@
 export const RECEIVE_STEP = 'RECEIVE_STEP';
 export const RECEIVE_STEPS = 'RECEIVE_STEPS';
-export const REMOVE_STEP = 'REMOVE_STEPS';
+export const REMOVE_STEP = 'REMOVE_STEP';
 
 export const receiveSteps = steps => ({
   type: RECEIVE_STEPS,

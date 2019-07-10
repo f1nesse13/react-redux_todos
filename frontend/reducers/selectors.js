804b6a309fee673e8e027fca36ccd969d652a88e
Joe Pell <jpell3130@gmail.com>
2019-07-10 12:12:05 -0400

    removed unneeded method and returned array from step by id

--- frontend/reducers/selectors.js
+++ frontend/reducers/selectors.js
@@ -1,10 +1,11 @@
 export const allTodos = ({ todos }) => Object.keys(todos).map(id => todos[id]);
 
 export const stepsByTodo = ({ steps }, todoid) => {
-  let matchingSteps = {};
+  let matchingSteps = [];
   Object.keys(steps).map(id => {
+    const step = steps[id];
     if (steps[id].todo_id === todoid) {
-      matchingSteps[id] = steps[id];
+      matchingSteps.push(step);
     }
   });
   return matchingSteps;
